85d140e2a6d999b300f93d351172328ffd7df20d
ahmed <aminebouthaleb@outlook.fr>
2022-01-22 05:07:16 +0100

    structure du projet complet

--- social-network/server.js
+++ social-network/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 require("dotenv").config({ path: "./config/.env" });
 const userRoutes = require("./routes/user.routes");
 const postRoutes = require("./routes/post.routes");
-const {checkUser, requireAuth} = require('./middelwares/mid');
+const { checkUser, requireAuth } = require("./middelwares/mid");
 const body_parser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const app = express();
 mongoose
@@ -19,16 +19,15 @@ app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: true }));
 app.use(cookieParser());
 //jwt-verification-token
-app.get('*',checkUser);
-app.get('/jwtid', requireAuth, (req, res) => {
-    res.status(200).send(res.locals.user._id)
-  });
+app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+    res.status(200).send(res.locals.user._id);
+});
 // routes
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 
-
 // server
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`Listening on port ${process.env.PORT} `);
 });
